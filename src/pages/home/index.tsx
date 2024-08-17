eea6a6b72a9de43931b7e983713e84c275e6b753
ali.kavakli <ali.kavakli4598@gmail.com>
2024-08-17 17:17:04 +0300

    update packages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,9 @@
-import reactLogo from "@/assets/react.svg"
 import React from "react"
 
 export default function HomePage() {
   const [count, setCount] = React.useState(0)
   return (
     <main className="h-screen flex flex-col items-center justify-center gap-4">
-      <div >
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} height={72} width={72} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1 className="text-2xl">Vite + React</h1>
       <div className="p-8 flex flex-col items-center justify-center gap-4">
         <button className="transition-all border border-zinc-500 p-2 rounded-md hover:bg-zinc-500 hover:border-white hover:text-white" onClick={() => setCount((count) => count + 1)}>
